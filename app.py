30f21733cdf23184296f5e6208a3ee2bc54e9b31
Tony Yang <superzoro168@gmail.com>
2017-08-27 12:47:45 +0800

    Error handling
    調整錯誤處理不要太複雜

--- app.py
+++ app.py
@@ -89,24 +89,13 @@ def next_page(vendor, page):
         try:
             urllib.request.urlretrieve(image_url, filepath)
             print(filename, image_url)
-        except (urllib.request.HTTPError, urllib.request.URLError, ValueError):
+        except (urllib.request.HTTPError, urllib.request.URLError, urllib.request.ContentTooShortError, ValueError):
             try:
                 urllib.request.urlretrieve(little_image_url, filepath)
                 print(filename, little_image_url)
-            except (urllib.request.HTTPError, urllib.request.URLError, ValueError):
+            except (urllib.request.HTTPError, urllib.request.URLError, urllib.request.ContentTooShortError, ValueError):
                 image.save(filepath, "PNG")
                 print(filename, 'empty image')
-        except urllib.request.ContentTooShortError:
-            try:
-                urllib.request.urlretrieve(image_url, filepath)
-                print(filename, image_url)
-            except (urllib.request.HTTPError, urllib.request.URLError, ValueError):
-                try:
-                    urllib.request.urlretrieve(little_image_url, filepath)
-                    print(filename, little_image_url)
-                except (urllib.request.HTTPError, urllib.request.URLError, ValueError):
-                    image.save(filepath, "PNG")
-                    print(filename, 'empty image')
 
     next_page(vendor, page + 1)
 
