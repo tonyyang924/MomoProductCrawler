9d6def8fea001f6ad48d7adcb7b790d510ed82e7
Tony Yang <superzoro168@gmail.com>
2018-04-08 22:48:36 +0800

    Fix TypeError & add logging module

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import time
 import json
 import urllib.request
 import sys
+import logging
 from pymongo import MongoClient
 from PIL import Image
 from selenium import webdriver
@@ -27,25 +28,23 @@ class Crawler:
     write_db_list = []
 
     def __init__(self, result_directory, dbtype):
-        # result_directory = 'result'
         self.result_directory = result_directory
         self.vendor_directory = result_directory + '/vendor'
-        self.create_directory(result_directory)
-        self.create_directory(self.vendor_directory)
+        create_directory(result_directory)
+        create_directory(self.vendor_directory)
         self.vendors = self.load_vendors()
         self.table_vendor = self.db.vendor
         if dbtype == DbType.MONGO.value:
             self.write_db_list.append(DbType.MONGO.value)
+        log_filename = "{}/{}.txt".format(result_directory, time.time())
+        logging.basicConfig(filename=log_filename, level=logging.DEBUG)
+        global logger
+        logger = logging.getLogger(__name__)
 
     def start(self):
         for vendor in self.vendors:
             self.crawler_vendor(vendor)
         self.driver.quit()
-
-    @staticmethod
-    def create_directory(path):
-        if not os.path.exists(path):
-            os.makedirs(path)
 
     @staticmethod
     def get_number(text):
@@ -69,13 +68,14 @@ class Crawler:
         return arr
 
     def crawler_vendor(self, vendor):
-        self.create_directory(self.vendor_directory + '/' + vendor)
+        create_directory(self.vendor_directory + '/' + vendor)
         self.trigger_click_page(vendor)
 
     def trigger_click_page(self, vendor):
         try:
             self.next_page(vendor, 1)
-        except WebDriverException:
+        except WebDriverException as err:
+            logging.error(err)
             print('『' + vendor + '』找不到下一頁的按鈕。')
 
     def get_vendor_max_page(self, vendor, page):
@@ -83,7 +83,8 @@ class Crawler:
             "//div[@class='pageArea']/ul/li/a")
         try:
             self.vendor_max_page = int(elements[-1].get_attribute('pageidx'))
-        except IndexError:
+        except IndexError as err:
+            logging.error(err)
             print("「{}」找不到頁數標籤，準備重整頁面並等待10秒...".format(vendor))
             self.driver.refresh()
             time.sleep(10)
@@ -106,7 +107,11 @@ class Crawler:
 
         directory = self.vendor_directory + '/' + vendor
 
-        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        try:
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        except TypeError as err:
+            logger.error(err)
+            return
         list_area = soup.find('div', {'class': 'listArea'}).find('ul')
         for item_li in list_area.select('li'):
             item = item_li.select_one('a.goodsUrl')
@@ -132,13 +137,17 @@ class Crawler:
             try:
                 urllib.request.urlretrieve(image_url, filepath)
                 print(filename, image_url)
-            except (urllib.request.HTTPError, urllib.request.URLError, urllib.request.ContentTooShortError, ValueError):
+            except (
+                    urllib.request.HTTPError, urllib.request.URLError, urllib.request.ContentTooShortError,
+                    ValueError) as err:
                 try:
+                    logging.error(err)
                     urllib.request.urlretrieve(little_image_url, filepath)
                     print(filename, little_image_url)
                 except (
                         urllib.request.HTTPError, urllib.request.URLError, urllib.request.ContentTooShortError,
-                        ValueError):
+                        ValueError) as err:
+                    logging.error(err)
                     self.image.save(filepath, "PNG")
                     print(filename, 'empty image')
 
@@ -163,6 +172,11 @@ class Instruction(Enum):
 
 class DbType(Enum):
     MONGO = "mongo"
+
+
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 
 def main():
